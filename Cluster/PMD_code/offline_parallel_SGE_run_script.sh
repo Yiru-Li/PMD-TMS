a290630de6b64b1dc85f549cb2655542adc2456e
Yiru-Li <125915608+Yiru-Li@users.noreply.github.com>
2026-04-17 11:44:54 -0400

    increase memory

--- Cluster/PMD_code/offline_parallel_SGE_run_script.sh
+++ Cluster/PMD_code/offline_parallel_SGE_run_script.sh
@@ -17,7 +17,7 @@ stage_3_max_walltime=${22}
 stage_4_max_walltime=${23}
 
 subj=$(basename $3)
-echo $subj
+echo "$subj started"
 
 #submit parallel jobs
 # while true; do
@@ -29,7 +29,7 @@ echo $subj
 # 	fi
 # done
 # while true; do
-    DP=$(qsub -N $subj-1.sh $1/offline_SGE.sh $1 ${24} offline_parallel_stage_1 \'$1\' $2 \'$3\' \'$4\' \'$5\' \'$6\' $7 \'$8\' \'$9\' ${10} \'${11}\' ${12} ${13} \'${14}\')
+    DP=$(qsub -N S$subj-1.sh -l h_vmem=64G,vf=64G,h_rt=01:00:00 $1/offline_SGE.sh $1 ${24} offline_parallel_stage_1 \'$1\' $2 \'$3\' \'$4\' \'$5\' \'$6\' $7 \'$8\' \'$9\' ${10} \'${11}\' ${12} ${13} \'${14}\')
 #     if [ "$?" = "0" ]; then
 # 		break
 # 	else
@@ -45,7 +45,7 @@ echo $subj
 # 	fi
 # done
 # while true; do
-    CR=$(qsub -N $subj-2.sh -hold_jid $subj-1.sh -t 1-$2 $1/offline_parallel_SGE.sh $1 ${24} offline_parallel_stage_2 $2 $7 SGE_TASK_ID \'$8\')
+    CR=$(qsub -N S$subj-2.sh -hold_jid S$subj-1.sh -t 1-$2 -l h_vmem=80G,vf=80G,h_rt=02:00:00 $1/offline_parallel_SGE.sh $1 ${24} offline_parallel_stage_2 $2 $7 SGE_TASK_ID \'$8\')
 #     if [ "$?" = "0" ]; then
 # 		break
 # 	else
@@ -61,7 +61,7 @@ echo $subj
 # 	fi
 # done
 # while true; do
-    CC=$(qsub -N $subj-3.sh -hold_jid $subj-2.sh -t 1-$2 $1/offline_SGE.sh $1 ${24} offline_parallel_stage_3 $2 $7 \'$8\')
+    CC=$(qsub -N S$subj-3.sh -hold_jid S$subj-2.sh -l h_vmem=32G,vf=32G,h_rt=01:00:00 $1/offline_SGE.sh $1 ${24} offline_parallel_stage_3 $2 $7 \'$8\')
 #     if [ "$?" = "0" ]; then
 # 		break
 # 	else
@@ -77,7 +77,7 @@ echo $subj
 # 	fi
 # done
 # while true; do
-    RC=$(qsub -N $subj-4.sh -hold_jid $subj-3.sh -t 1-$2 $1/offline_parallel_SGE.sh $1 ${24} offline_parallel_stage_4 $2 $7 SGE_TASK_ID \'$8\')
+    RC=$(qsub -N S$subj-4.sh -hold_jid S$subj-3.sh -t 1-$2 -l h_vmem=32G,vf=32G,h_rt=01:00:00 $1/offline_parallel_SGE.sh $1 ${24} offline_parallel_stage_4 $2 $7 SGE_TASK_ID \'$8\')
 #     if [ "$?" = "0" ]; then
 # 		break
 # 	else
